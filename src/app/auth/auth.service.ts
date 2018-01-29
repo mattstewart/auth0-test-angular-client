2ec78b367d00c9ea83e7e11021bfd809bf7232fa
Matt Stewart <matt@mattstewart.io>
2018-01-29 08:26:47 -0900

    Added missing Angular CLI file

--- src/app/auth/auth.service.ts
+++ src/app/auth/auth.service.ts
@@ -12,8 +12,7 @@ export class AuthService {
     responseType: 'token id_token',
     audience: `https://${AUTH_CONFIG.domain}/userinfo`,
     redirectUri: AUTH_CONFIG.callbackURL,
-    scope: 'openid',
-    sso: true
+    scope: 'openid'
   });
 
   constructor(public router: Router) {}
